55b5400d4f566da8a628f428f60fd66b8555216f
Hardik Desai <hardikd@zignuts.com>
2023-05-15 16:43:52 +0530

    perf: :ambulance: Prop Types

---
+++ react-typescript/src/components/AppBar.tsx
@@ -0,0 +1,31 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "./../logo.svg";
+
+type NavType = {
+  name: string;
+};
+function AppBar(props: NavType) {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">{props.name}</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Brand href="#home">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default AppBar;
